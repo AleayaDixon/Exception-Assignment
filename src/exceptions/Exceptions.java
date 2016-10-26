70b64d0467f49c851b7642c9ab52f9464b3f6e95
Aleaya <Aleaya@AcerLaptop.howardu.net>
2016-10-26 16:01:05 -0400

    exceptions

---
+++ src/exceptions/Exceptions.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exceptions;
+import java.util.Scanner;
+import java.lang.ArithmeticException;
+
+/**
+ *
+ * @author Aleaya
+ */
+public class Exceptions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a, b;
+        int result = 0;
+ 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input two integers");
+        
+        try
+        {
+            a = input.nextInt();
+            b = input.nextInt();
+            result = a / b;
+            System.out.println("Result = " + result);
+        }catch(ArithmeticException e)
+        {
+            System.out.println("Something is wrong");
+        }              
+    }
+    
+}
